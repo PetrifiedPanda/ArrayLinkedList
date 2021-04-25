3fe08c02106fc3300be3f46ebf3e07af7df74841
PetrifiedPanda <bfj.koehn@gmail.com>
2021-04-25 21:26:03 +0200

    Initial commit

---
+++ ArrayLinkedList.h
@@ -0,0 +1,478 @@
+#pragma once
+
+#include <cstddef>
+
+template <typename T>
+class ArrayLinkedList {
+    class Node {
+       public:
+        T* keys;
+
+        Node* next;
+        Node* prev;
+
+        Node(size_t alloc_size, Node* prev = nullptr) :
+            keys(new T[alloc_size]), 
+            next(nullptr), 
+            prev(prev) {}
+
+        ~Node() {
+            delete[] keys;
+        }
+    };
+
+    static const size_t s_default_node_size_ = 50;
+
+    Node* head_;
+    Node* tail_;
+
+    size_t node_size_;
+    size_t node_count_;
+    size_t tail_size_;
+
+    static void free_following_nodes(Node* start) {
+        Node* it = start;
+        while (it != nullptr) {
+            Node* tmp = it;
+            it = it->next;
+            delete tmp;
+        }
+    }
+
+    void _free() {
+        free_following_nodes(head_);
+    }
+
+    static void copy_arr(T* to, T* from, size_t size) {
+        for (size_t i = 0; i < size; ++i)
+            to[i] = from[i];
+    }
+
+    void append_following_nodes(Node* copy_begin) {
+        if (head_ == nullptr)
+            head_ = tail_ = new Node(node_size_);
+        else {
+            tail_->next = new Node(node_size_, tail_);
+            tail_ = tail_->next;
+        }
+
+        for (Node* it = copy_begin; it != nullptr; it = it->next) {
+            if (it->next != nullptr) {
+                copy_arr(tail_->keys, it->keys, node_size_);
+                tail_->next = new Node(node_size_, tail_);
+                tail_ = tail_->next;
+            } else {
+                copy_arr(tail_->keys, it->keys, tail_size_);
+            }
+        }
+    }
+
+    void _copy_same_node_size(const ArrayLinkedList<T>& other) {
+        node_count_ = other.node_count_;
+        tail_size_ = other.tail_size_;
+        Node* it = head_;
+        Node* other_it = other.head_;
+
+        while (it != nullptr && other_it != nullptr) {
+            size_t copy_size = other_it->next == nullptr ? tail_size_ : node_size_;
+            copy_arr(it->keys, other_it->keys, copy_size);
+
+            it = it->next;
+            other_it = other_it->next;
+        }
+
+        if (other_it == nullptr && it != nullptr) {
+            tail_ = it->prev;
+            tail_->next = nullptr;
+            free_following_nodes(it);
+        } else if (other_it != nullptr && it == nullptr) {
+            append_following_nodes(other_it);
+        }
+    }
+
+    void _copy(const ArrayLinkedList<T>& other) {
+        node_size_ = other.node_size_;
+        node_count_ = other.node_count_;
+        tail_size_ = other.tail_size_;
+
+        // This is so head does not stay uninitialised in the function call
+        head_ = nullptr;
+        append_following_nodes(other.head_);
+    }
+
+    void _move(ArrayLinkedList<T>&& other) {
+        node_size_ = other.node_size_;
+        node_count_ = other.node_count_;
+        tail_size_ = other.tail_size_;
+        head_ = other.head_;
+        tail_ = other.tail_;
+
+        other.head_ = nullptr;
+        other.tail_ = nullptr;
+        other.node_count_ = 0;
+        other.tail_size_ = 0;
+    }
+
+   public:
+    ArrayLinkedList(size_t node_size = s_default_node_size_) :
+        head_(nullptr),
+        tail_(nullptr),
+        node_size_(node_size),
+        node_count_(0),
+        tail_size_(0) {}
+
+    ArrayLinkedList(const ArrayLinkedList<T>& other) {
+        _copy(other);
+    }
+
+    ArrayLinkedList(ArrayLinkedList<T>&& other) {
+        _move(std::move(other));
+    }
+
+    ~ArrayLinkedList() {
+        _free();
+    }
+
+    ArrayLinkedList<T>& operator=(const ArrayLinkedList<T>& other) {
+        if (node_size_ == other.node_size_)
+            _copy_same_node_size(other);
+        else {
+            _free();
+            _copy(other);
+        }
+        return *this;
+    }
+
+    ArrayLinkedList<T>& operator=(ArrayLinkedList<T>&& other) {
+        _free();
+        _move(std::move(other));
+        return *this;
+    }
+
+    size_t node_size() const {
+        return node_size_;
+    }
+
+    size_t size() const {
+        if (node_count_ == 0)
+            return 0;
+        else
+            return (node_count_ - 1) * node_size_ + tail_size_;
+    }
+
+    bool empty() const {
+        return size() == 0;
+    }
+
+    T& front() {
+        return head_->keys[0];
+    }
+
+    const T& front() const {
+        return head_->keys[0];
+    }
+
+    T& back() {
+        return tail_->keys[tail_size_ - 1];
+    }
+
+    const T& back() const {
+        return tail_->keys[tail_size_ - 1];
+    }
+
+    void clear() {
+        _free();
+        head_ = tail_ = nullptr;
+    }
+
+   private:
+    T& get_item_at_index(size_t index) const {
+        if (index < size()) {
+            size_t node_number = index / node_size_;
+
+            Node* it = head_;
+            for (size_t i = 0; i < node_number; ++i)
+                it = it->next;
+            
+            return it->keys[index - node_number * node_size_];
+        } else {
+            throw std::runtime_error("Index out of bounds");
+        }
+    }
+
+   public:
+    T& at(size_t index) {
+        return get_item_at_index(index);
+    }
+
+    const T& at(size_t index) const {
+        return get_item_at_index(index);
+    }
+
+    void resize(size_t new_size, const T& fill_item = T()) {
+	    if (new_size < size()) {
+            while (size()  > new_size) {
+                if (size() - tail_size_ >= new_size) {
+                    remove_last_node();
+                } else {
+                    size_t size_difference = size() - new_size;
+                    tail_size_ -= size_difference;
+                }
+            }
+        } else {
+            while (size() < new_size) {
+                if (tail_size_ == node_size_) {
+                    tail_->next = new Node(node_size_, tail_);
+                    tail_ = tail_->next;
+                    ++node_count_;
+                    tail_size_ = 0;
+                }
+                tail_->keys[tail_size_] = fill_item;
+                ++tail_size_;
+            }
+        }
+    }
+   private:
+    /*
+    Implements logic for appending a new element. The actual insertion is passed as a function that takes the node where the key is to be inserted
+    so the same logic isn't repeated in three separate functions
+    */
+    template <typename Function>
+    void push_back_template(Function func) {
+        if (head_ == nullptr) {
+            head_ = tail_ = new Node(node_size_);
+            node_count_ = 1;
+            func(head_);
+        } else if (tail_size_ < node_size_) {
+            func(tail_);
+        } else {
+            tail_->next = new Node(node_size_, tail_);
+            ++node_count_;
+            tail_ = tail_->next;
+            tail_size_ = 0;
+            func(tail_);
+        }
+    }
+
+   public:
+
+    void push_back(const T& key) {
+        push_back_template([&](Node* node) {
+            node->keys[tail_size_] = key;
+            ++tail_size_;
+        });
+    }
+
+    void push_back(T&& key) {
+        push_back_template([&](Node* node) {
+            node->keys[tail_size_] = std::move(key);
+            ++tail_size_;
+        });
+    }
+
+    template <typename... Args>
+    void emplace_back(Args... args) {
+        push_back_template([&](Node* node) {
+            node->keys[tail_size_] = T(args...);
+            ++tail_size_;
+        });
+    }
+
+   private:
+
+    void remove_last_node() {
+        Node* prev_tail = tail_;
+        tail_ = tail_->prev;
+        if (tail_ == nullptr) {
+            tail_size_ = 0;
+            head_ = nullptr;
+        } else {
+            tail_->next = nullptr;
+            tail_size_ = node_size_;
+        }
+        delete prev_tail;
+        --node_count_;
+    }
+
+   public:
+
+    void pop_back() {
+        if (tail_size_ > 1) {
+            --tail_size_;
+        }
+        else {
+            remove_last_node();
+        }
+    }
+
+    class const_iterator {
+        friend class ArrayLinkedList<T>;
+       protected:
+        Node* current_node_;
+        size_t index_;
+        size_t node_size_;
+        const size_t* tail_size_;
+
+        const_iterator(Node* currentNode, size_t index, size_t node_size, const size_t* tail_size) : 
+            current_node_(currentNode), 
+            index_(index),
+            node_size_(node_size),
+            tail_size_(tail_size) {}
+       public:
+        const_iterator() : 
+            current_node_(nullptr), 
+            index_(0), 
+            node_size_(0), 
+            tail_size_(nullptr) {}
+
+        const_iterator& operator++() {
+            if ((current_node_->next == nullptr && index_ < *tail_size_ - 1) || (current_node_->next != nullptr && index_ < node_size_ - 1)) {
+                ++index_;
+            } else {
+                current_node_ = current_node_->next;
+                index_ = 0;
+            }
+            return *this;
+        }
+
+        const_iterator& operator--() {
+            if (index_ > 0) {
+                --index_;
+            } else {
+                current_node_ = current_node_->prev;
+                index_ = node_size_ - 1;
+            }
+            return *this;
+        }
+
+        bool operator==(const const_iterator& other) const {
+            return current_node_ == other.current_node_ && index_ == other.index_;
+        }
+
+        bool operator!=(const_iterator other) const {
+            return !(*this == other);
+        }
+
+        const T& operator*() const {
+            return current_node_->keys[index_];
+        }
+
+        const T* operator->() const {
+            return &current_node_->keys[index_];
+        }
+    };
+
+    const_iterator cbegin() const {
+        return const_iterator(head_, 0, node_size_, &tail_size_);
+    }
+
+    const_iterator cend() const {
+        return const_iterator(nullptr, 0, node_size_, &tail_size_);
+    }
+
+    const_iterator begin() const {
+        return cbegin();
+    }
+
+    const_iterator end() const {
+        return cend();
+    }
+
+    class iterator : public const_iterator {
+        friend class ArrayLinkedList<T>;
+
+        iterator(Node* currentNode, size_t index, size_t node_size, const size_t* tail_size) : 
+            const_iterator(currentNode, index, node_size, tail_size) {}
+       public:
+        T& operator*() {
+            return this->current_node_->keys[this->index_];
+        }
+
+        T* operator->() {
+            return &this->current_node_->keys[this->index_];
+        }
+    };
+
+    iterator begin() {
+        return iterator(head_, 0, node_size_, &tail_size_);
+    }
+
+    iterator end() {
+        return iterator(nullptr, 0, node_size_, &tail_size_);
+    }
+
+    // TODO:: reverse iterators
+
+   private:
+    /*
+    Implementation of logic for searching, for use with different iterator types
+    Returns a node pointer and the index of the key in the given node
+    */
+    std::pair<Node*, size_t> find_key(const T& key) const {
+        for (Node* it = head_; it != nullptr; it = it->next) {
+            size_t size = it->next == nullptr ? tail_size_ : node_size_;
+            for (size_t i = 0; i < size; ++i) {
+                if (it->keys[i] == key)
+                    return std::make_pair(it, i);
+            }
+        }
+
+        return std::make_pair(nullptr, 0);
+    }
+
+   public:
+    const_iterator find(const T& key) const {
+        auto [node, index] = find_key(key);
+        return const_iterator(node, index, node_size_, &tail_size_);
+    }
+
+    iterator find(const T& key) {
+        auto [node, index] = find_key(key);
+        return iterator(node, index, node_size_, &tail_size_);
+    }
+
+    bool contains(const T& key) const {
+        return find(key) != end();
+    }
+
+   private:
+
+    static void shift_one_back(T* arr, size_t start_index, size_t size) {
+        for (size_t i = start_index; i < size; ++i)
+            arr[i - 1] = std::move(arr[i]);
+    }
+
+   public:
+
+    // returns the iterator following the removed item
+    iterator erase(iterator pos) { // TODO: const_iterator alternative
+        size_t start_node_size = pos.current_node_->next == nullptr ? tail_size_ : node_size_;
+        shift_one_back(pos.current_node_->keys, pos.index_ + 1, start_node_size);
+
+        if (pos.current_node_->next != nullptr) {
+            Node* it = pos.current_node_;
+            while (it->next != nullptr) {
+                it->keys[node_size_ - 1] = std::move(it->next->keys[0]);
+                shift_one_back(it->next->keys, 1, node_size_);
+
+                it = it->next;
+            }
+
+            shift_one_back(it->keys, 1, tail_size_);
+        }
+
+        --tail_size_;
+        if (tail_size_ == 0) {
+            bool return_end = false;
+            if (pos.current_node_ == tail_)
+                return_end = true;
+            remove_last_node();
+
+            if (return_end)
+                return end();
+        }
+        return pos;
+    }
+
+    // TODO: Range deletion
+};
---
+++ test/ArrayLinkedListTest.cpp
@@ -0,0 +1,230 @@
+#include <gtest/gtest.h>
+
+#include "../ArrayLinkedList.h"
+
+struct ArrayLinkedListTest : public testing::Test {
+    ArrayLinkedList<int> list;
+
+    virtual void SetUp() override {
+        for (int i = 0; i < 50; ++i)
+            list.push_back(i);
+
+        for (int i = 0; i < 50; ++i)
+            list.emplace_back(i);
+
+        list.push_back(10000);
+        // Should create 3 nodes
+    }
+};
+
+template <typename ItType>
+void iterator_test(ArrayLinkedList<int>& list) {
+    ItType it = list.begin();
+
+    for (int i = 0; i < 50; ++i) {
+        ASSERT_EQ(*it, i);
+        ++it;
+    }
+
+    // Push back item while iterating
+    list.push_back(30);
+
+    for (int i = 0; i < 50; ++i) {
+        ASSERT_EQ(*it, i);
+        ++it;
+    }
+
+    ASSERT_EQ(*it, 10000);
+    ++it;
+    ASSERT_EQ(*it, 30);
+
+    ItType itCopy = it;
+    ++itCopy;
+    ASSERT_EQ(itCopy, list.end());
+    ----it;
+
+    // Go through backwards to test decrementing
+
+    for (int i = 49; i > -1; --i) {
+        ASSERT_EQ(*it, i);
+        --it;
+    }
+
+    // revert the push_back from before so the list is in the same state as before the function call
+    list.pop_back(); 
+    ASSERT_EQ(list.back(), 10000);
+
+    for (int i = 49; i > -1; --i) {
+        ASSERT_EQ(*it, i);
+        if (i != 0)
+            --it;
+    }
+
+    ASSERT_EQ(it, list.begin());
+}
+
+
+TEST_F(ArrayLinkedListTest, Iterators) {
+    iterator_test<ArrayLinkedList<int>::iterator>(list);
+
+    iterator_test<ArrayLinkedList<int>::const_iterator>(list);
+}
+
+TEST_F(ArrayLinkedListTest, Resize) {
+    int fill_item = 69;
+    size_t added_elements = 50;
+    size_t prev_list_size = list.size();
+    list.resize(list.size() + added_elements, fill_item);
+
+    auto it = list.begin();
+
+    while (*it != fill_item)
+        ++it;
+
+    size_t count = 0;
+    while (it != list.end()) {
+        EXPECT_EQ(*it, fill_item);
+        ++count;
+        ++it;
+    }
+
+    EXPECT_EQ(count, added_elements);
+
+    list.resize(prev_list_size);
+
+    // Run the iterator test again after resizing the list to the same size as before
+    iterator_test<ArrayLinkedList<int>::iterator>(list);
+}
+
+TEST_F(ArrayLinkedListTest, Copy) {
+    ArrayLinkedList<int> copy(list);
+
+    iterator_test<ArrayLinkedList<int>::iterator>(copy);
+    // Change copy and check if the original has changed
+    for (int& key : copy)
+        key = 100;
+
+    for (int i = 0; i < 142; ++i)
+        copy.push_back(100);
+    
+    iterator_test<ArrayLinkedList<int>::iterator>(list);
+
+    ArrayLinkedList<int> copy_of_copy(copy);
+    // Assign copy to list again to test copy assignment (case that other.size() < this->size() in _copy_same_node_size())
+    copy = list; 
+    iterator_test<ArrayLinkedList<int>::iterator>(copy);
+
+    // case that other.size() > this->size() in _copy_same_node_size()
+    copy = copy_of_copy;
+
+    for (const int& key : copy)
+        EXPECT_EQ(key, 100);
+}
+
+TEST_F(ArrayLinkedListTest, Move) {
+    // Assuming that copy is correct we can compare the move to the copy
+    ArrayLinkedList<int> copy(list); 
+    ArrayLinkedList<int> move(std::move(list));
+
+    // Check if list is in a valid state (should be empty)
+    EXPECT_EQ(list.size(), 0);
+
+    auto copy_it = copy.begin();
+    auto move_it = move.begin();
+    while (copy_it != copy.end() && move_it != move.end()) {
+        EXPECT_EQ(*move_it, *copy_it);
+        ++copy_it;
+        ++move_it;
+    }
+
+    // fill list to test move assignment into non-empty list
+    for (int i = 0; i < 231; ++i)
+        list.push_back(rand());
+
+    list = std::move(move);
+    iterator_test<ArrayLinkedList<int>::iterator>(list);
+}
+
+TEST_F(ArrayLinkedListTest, Contains) {
+    for (int i = 0; i < 50; ++i)
+        EXPECT_TRUE(list.contains(i));
+
+    EXPECT_TRUE(list.contains(10000));
+
+    EXPECT_FALSE(list.contains(6234));
+}
+
+TEST_F(ArrayLinkedListTest, Find) {
+    auto it = list.find(3);
+    EXPECT_EQ(*it, 3);
+    
+    --it;
+    EXPECT_EQ(*it, 2);
+
+    ++++it;
+    EXPECT_EQ(*it, 4);
+
+    it = list.find(10000);
+    EXPECT_EQ(*it, 10000);
+    auto itCopy = it;
+    ++itCopy;
+    EXPECT_EQ(itCopy, list.end());
+
+    auto invalid_it = list.find(234243);
+    EXPECT_EQ(invalid_it, list.end());
+}
+
+TEST_F(ArrayLinkedListTest, Indexing) {
+    for (size_t i = 0; i < list.size(); ++i) {
+        auto& key = list.at(i);
+        if (i < 50)
+            EXPECT_EQ(key, i);
+        else if (i < 100)
+            EXPECT_EQ(key, i - 50);
+        else
+            EXPECT_EQ(key, 10000);
+
+        key = 100;
+    }
+
+    // For testing the const implementation of at()
+    const auto& list_ref = list;
+    for (size_t i = 0; i < list.size(); ++i) {
+        EXPECT_EQ(list_ref.at(i), 100);
+    }
+}
+
+TEST_F(ArrayLinkedListTest, Erase) {
+    size_t prev_size = list.size();
+    auto it = list.find(40);
+    auto after_it = list.erase(it);
+    EXPECT_EQ(*after_it, 41);
+
+    for (int i = 41; i < 50; ++i) {
+        EXPECT_EQ(*after_it, i);
+        ++after_it;
+    }
+
+    for (int i = 0; i < 50; ++i) {
+        EXPECT_EQ(*after_it, i);
+        ++after_it;
+    }
+
+    EXPECT_EQ(*after_it, 10000);
+    ++after_it;
+    EXPECT_EQ(after_it, list.end());
+
+    EXPECT_EQ(list.size(), prev_size - 1);
+}
+
+TEST_F(ArrayLinkedListTest, Size) {
+    size_t prev_size = list.size();
+    EXPECT_EQ(prev_size, 101);
+
+    for (int i = 0; i < prev_size; ++i) {
+        EXPECT_EQ(list.size(), prev_size - i);
+        list.pop_back();
+    }
+
+    EXPECT_EQ(list.size(), 0);
+}
